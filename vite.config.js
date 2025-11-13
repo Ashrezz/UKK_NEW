95db3f8c9721d9053ef7140573cfe60058ac2d01
Marcel <iwayansaputramarchel@gmail.com>
2025-11-13 19:52:02 +0700

    fix: remove cache lock issue by clearing node_modules/.cache and adding --force flag

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite'
+import laravel from 'laravel-vite-plugin'
 
 export default defineConfig({
+    // 👇 Pindahkan cache keluar dari node_modules
+    cacheDir: './.vitecache',
+
     plugins: [
-            laravel({
-            input: ['resources/css/app.css', 'resources/css/layout-redesign.css', 'resources/js/app.js'],
+        laravel({
+            input: [
+                'resources/css/app.css',
+                'resources/css/layout-redesign.css',
+                'resources/js/app.js'
+            ],
             refresh: true,
         }),
     ],
-});
+})
